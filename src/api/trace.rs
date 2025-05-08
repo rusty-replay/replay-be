14f1704c870d07a095cddb040b4981bdbf7a4c02
Jaeha Lee <wogkdkrm112@gmail.com>
2025-05-08 22:39:06 +0900

    chore: minor update

--- src/api/trace.rs
+++ src/api/trace.rs
@@ -6,7 +6,6 @@ use opentelemetry_proto::tonic::common::v1::any_value::Value as AnyValueKind;
 
 #[post("/traces")]
 async fn receive_traces(body: Bytes) -> impl Responder {
-    // Protobuf 바이트를 디코딩
     let req = match ExportTraceServiceRequest::decode(body.as_ref()) {
         Ok(r) => r,
         Err(err) => {
@@ -17,32 +16,28 @@ async fn receive_traces(body: Bytes) -> impl Responder {
 
     for resource_spans in req.resource_spans {
         for scope_spans in resource_spans.scope_spans {
-            for span in scope_spans.spans {
+            for span in &scope_spans.spans {
                 let attrs: Vec<(String, Option<String>)> = span
                     .attributes
-                    .into_iter()
+                    .iter()
                     .map(|kv| {
                         let val = kv
                             .value
-                            .and_then(|any| any.value)
+                            .as_ref()
+                            .and_then(|any| any.value.as_ref())
                             .and_then(|kind| match kind {
-                                AnyValueKind::StringValue(s) => Some(s),
+                                AnyValueKind::StringValue(s) => Some(s.clone()),
                                 AnyValueKind::IntValue(i)    => Some(i.to_string()),
                                 AnyValueKind::DoubleValue(d) => Some(d.to_string()),
                                 AnyValueKind::BoolValue(b)   => Some(b.to_string()),
                                 _ => None,
                             });
-                        (kv.key, val)
+                        (kv.key.clone(), val)
                     })
                     .collect();
 
-                println!(
-                    "Span received: name={} start={} end={} attrs={:?}",
-                    span.name,
-                    span.start_time_unix_nano,
-                    span.end_time_unix_nano,
-                    attrs,
-                );
+                println!("Span attrs: {:?}", attrs);
+                println!("Full span:\n{:#?}", span);
             }
         }
     }
